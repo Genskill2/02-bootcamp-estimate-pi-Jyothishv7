f5f083b67550f5bf7a52039599ed400268cc51ae
Jyothishv7 <61043734+Jyothishv7@users.noreply.github.com>
2021-05-29 22:28:02 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -26,6 +26,32 @@ class TestMC(unittest.TestCase):
         for i in range(500, 600):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+            
+            
+def wallis(n):
+    i = 1
+    val = 1
+    tot = 0
+    while i <= n:
+        val = 2*((4 * i * i) / ((4 * i * i) - 1))
+        tot = tot + val
+        i = i + 1
+    return tot
+def monte_carlo(n):
+    i=1
+    inspoint = 0
+    while i<=n:
+        x= random.random()
+        y= random.random()
+        distance = math.sqrt((x** 2) + (y** 2))
+        if distance<1:
+            inspoint = inspoint+1
+        i = i + 1
+    val=4*(inspoint/n)
+    return val
+        
+        
+           
         
     
 if __name__ == "__main__":
